fdd522ca7ed08b88da030eb7c23b8f08e36ba3b6
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2020-06-28 15:59:30 +0600

    Animation 3 with JS created

---
+++ Animation - 3/javaScript.js
@@ -0,0 +1,10 @@
+function showLine()
+{
+    var div=document.getElementById("container2");
+    div.style.display='block';
+}
+function hideLine()
+{
+    var elem=document.getElementById("container2");
+    elem.style.display='none';
+}
